c85c1c650dbcdba72e7fb61d6fa5221bd0b69c70
Mikkel RINGAUD <mikkel@milescode.dev>
2024-03-20 19:15:25 +0100

    chore: add icons and kobalte

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 
+import icons from "unplugin-icons/vite";
 import solid from 'vite-plugin-solid';
 import unocss from 'unocss/vite';
+
+import path from "node:path";
 
 export default defineConfig({
   plugins: [
     unocss(),
+    icons({ compiler: 'solid' }),
     solid()
-  ]
+  ],
+
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src")
+    }
+  },
 });
